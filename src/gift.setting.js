49a0c6cc0a1f2bbed9fdb529d63451d9c2e8dbe6
daweiyuanzhang <31055033+daweiyuanzhang@users.noreply.github.com>
2024-08-02 00:44:37 -0500

    Update gift.setting.js

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -15,22 +15,21 @@ export const RESTART_COUNT = 5;
 // 首页配置
 export const homeConfig = {
   // 标题
-  title: '我们结婚一周年啦！',
+  title: '璐璐专属来啦',
   // 留言，数组的每一项代表一行
   messages: [
-    { key: 'a', wording: '虽然来的有些迟了', },
+    { key: 'a', wording: '可能不够惊喜', },
     { key: 's', wording: '但希望你能喜欢', },
-    { key: 'd', wording: '结婚后，依然可以浪漫', },
   ],
   // 最终解释权归属人
-  owner: '启先森',
+  owner: '宇峰',
   // 抽奖转盘的动画时间，单位毫秒
   timeout: 3000,
   // 指定赠送的礼物的索引（第几个礼物中奖），取值范围 0-8
   targetGiftIndex: null,
 
   // 指定礼物顺序
-  targetGiftIndexList: [0, 2, 4, 6, 7],
+  targetGiftIndexList: [0, 3, 5, 6],
 };
 
 // 礼物清单
@@ -38,12 +37,12 @@ export const homeConfig = {
 // 图片放在 public/images 中，建议是正方形，否则影响视觉体验
 // name 是礼物的全称；alias 是礼物的简称（用来显示在抽奖方格里）
 export const gifts = [
-  { key: 'q', name: '一周年纪念木刻', alias: '纪念木刻', image: '/images/wood.png',  description: '「始于初见，止于终老」' },
-  { key: 'w', name: 'DIOR 迪奥小姐玫舞轻旋淡香水', alias: 'DIOR香水', image: '/images/dior.png',  description: '“每一天都是香香的”' },
-  { key: 'e', name: '一周年纪念小册', alias: '纪念小册', image: '/images/book.png',  description: '「余生有你，何其欢喜」' },
-  { key: 'r', name: 'EOS R10 相机', alias: 'R10', image: '/images/eosr10.png',  description: '“留下你的每一份美丽”' },
-  { key: 't', name: 'AirPods pro 2', alias: 'Airpods', image: '/images/airpods2.png',  description: '「特别的一刻，只属于你」' },
-  { key: 'y', name: '1314红包', alias: '1314', image: '/images/6.png',  description: '“从今往后，我都会在你旁边”' },
-  { key: 'u', name: 'IPhone 15 Pro', alias: 'IPhone15Pro', image: '/images/ipone15pro.png',  description: '“是时候换个新手机啦！”' },
-  { key: 'i', name: 'NOE BB', alias: 'LV女包', image: '/images/lvbag.png',  description: '「何以解忧，唯有包包」' },
+  { key: 'q', name: '全身按摩(30min)', alias: '按摩(30min)', image: './images/spa.png',  description: '「奢华享受, 感受轻松」' },
+  { key: 'i', name: 'LAMER精华乳霜30ml', alias: 'LAMER乳霜', image: './images/lamer.png',  description: '「柔韧修护, 纵享年轻」' },
+  { key: 'r', name: 'Sony XT5相机', alias: 'R10', image: '/images/xt5.png',  description: '“留下你的每一份美丽”' },
+  { key: 'w', name: '全定制旅行', alias: '全定制旅行', image: './images/travel.png',  description: '「说走就走, 私享定制」' },
+  { key: 't', name: '竹哩大餐一顿', alias: '竹哩', image: '/images/zhuli.png',  description: '「只要你喜欢，我就陪你去」' },
+  { key: 'y', name: '1314红包', alias: '1314', image: '/images/6.png',  description: '“我会永远在你旁边”' },
+  { key: 'u', name: '赫莲娜黑绷带', alias: '赫莲娜', image: './images/helianna.png',  description: '「极致呵护, 轻松自在」' },
+  { key: 'i', name: '黄金(10g)', alias: '黄金(10g)', image: './images/gold.png',  description: '「金诚可贵, 情比金坚」' },
 ];
